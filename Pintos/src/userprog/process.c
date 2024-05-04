5cc93b6381f6813d8f50d663e369b3ba20dc4202
Ziad ELsharkawy <ziadelsharkawy702@gmail.com>
2024-05-05 01:45:22 +0300

    Initializing System Calls

--- Pintos/src/userprog/process.c
+++ Pintos/src/userprog/process.c
@@ -17,7 +17,7 @@
 #include "threads/palloc.h"
 #include "threads/thread.h"
 #include "threads/vaddr.h"
-
+//hn3ml implement ll excute system call --ziad
 static thread_func start_process NO_RETURN;
 static bool load (const char *cmdline, void (**eip) (void), void **esp);
 
--- Pintos/src/userprog/syscall.c
+++ Pintos/src/userprog/syscall.c
@@ -4,17 +4,46 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 
-static void syscall_handler (struct intr_frame *);
+static void syscall_handler(struct intr_frame *);
 
-void
-syscall_init (void) 
+void syscall_init(void)
 {
-  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+  intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler(struct intr_frame *f ) // 1.halt 2.exit 3.exec 4.wait 5.create 6.remove 7.open 8.filesize 9.read 10.write 11.seek 12.tell 13.close
 {
-  printf ("system call!\n");
-  thread_exit ();
+  int syscall = &f->esp;
+  // printf ("system call!\n");
+  if (syscall == SYS_HALT){
+
+  } else if (syscall == SYS_EXIT){
+
+  }else if (syscall == SYS_EXEC){
+
+  }else if (syscall == SYS_WAIT){
+
+  }else if (syscall == SYS_CREATE){
+
+  }else if (syscall == SYS_REMOVE){
+
+  }else if (syscall == SYS_OPEN){
+
+  }else if (syscall == SYS_FILESIZE){
+
+  }else if (syscall == SYS_READ){
+
+  }else if (syscall == SYS_WRITE){
+
+  }else if (syscall == SYS_SEEK){
+
+  }else if (syscall == SYS_TELL){
+
+  }else if (syscall == SYS_CLOSE){
+
+  }
+//     handle_exit();
+// }
+thread_exit();
 }
